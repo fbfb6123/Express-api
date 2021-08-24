fdcb681340b0151d1b447106d7798713c6c12e10
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-08-24 16:21:41 +0900

    ディレクトリ整理

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const app = express()
+// ライブラリ読み込み
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//body-parserの設定
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var port = process.env.PORT || 3000; // port番号を指定
+
+
+// GET http://localhost:3000/api/v1/
+var router = require('./routes/v1/'); ///routes/v1/は/routes/v1/index.jsとして扱われる
+app.use('/api/v1/', router);
+
+//サーバ起動
+app.listen(port);
+console.log('listen on port ' + port);
